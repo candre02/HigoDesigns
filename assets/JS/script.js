c056735769a1429697b75380ad1cf75536fb48f7
Crystal Andre <candre02.2@gmail.com>
2023-03-20 14:00:04 -0500

    edited script

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,17 +1,19 @@
 
-
-function Shopping() {
+<script>
+function Shopping() = {
     // create a button element
-    const cart_btn = document.createElement('cart_btn')
+    cart_btn = document.createElement('cart_btn')}
     // set the button text to 'Add to Cart'
-    cart_btn.innerText = 'Add to Cart'
+    cart_btn.innerText = 'Add to Cart';
     // Attach the "click" event to your cart_btn
-    cart_btn.addEventListener('click', () => {
+    cart_btn.addEventListener('click', () = {
         // where there is a "click"
         // it shows an alert in the browser
         alert('Oh, you clicked me!')
-    })
-    // Add the button to your HTML <body> tag 
-    document.body.appendChild(cart_btn)
-}
+    });
+    // Add the button to your HTML 
+    document.body.appendChild(cart_btn);
 
+Shopping();
+
+</script>
