b66c6440cdfadda539274b2b85e7b86f9b3bc312
Crystal Andre <candre02.2@gmail.com>
2023-03-20 13:42:16 -0500

    added script for the file, html, and edited the class to type on the button add to cart

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,17 @@
-// // getting all required elements
-// const cart_btn = document.querySelector(".cart_btn button");
-// const cards_box = document.querySelector(".cards_box");
 
-// // If add to cart button is clicked 
-// cart_btn.onclick = () => {
-//     cards_box.classList.add("activeInfo"); // show the add to cart
-// }
 
+function Shopping() {
+    // create a button element
+    const cart_btn = document.createElement('cart_btn')
+    // set the button text to 'Add to Cart'
+    cart_btn.innerText = 'Add to Cart'
+    // Attach the "click" event to your cart_btn
+    cart_btn.addEventListener('click', () => {
+        // where there is a "click"
+        // it shows an alert in the browser
+        alert('Oh, you clicked me!')
+    })
+    // Add the button to your HTML <body> tag 
+    document.body.appendChild(cart_btn)
+}
 
